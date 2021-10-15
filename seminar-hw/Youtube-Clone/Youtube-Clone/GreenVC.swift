4c82dcddf1f849320746e9e5ee56e6aa989e3e63
hellozo0 <ahju0011@gmail.com>
2021-10-15 19:54:25 +0900

    ✨[FEAT] : 2차 과제 Tabbar 생성(#5)

---
+++ seminar-hw/Youtube-Clone/Youtube-Clone/GreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  GreenVC.swift
+//  Youtube-Clone
+//
+//  Created by 안현주 on 2021/10/15.
+//
+
+import UIKit
+
+class GreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
